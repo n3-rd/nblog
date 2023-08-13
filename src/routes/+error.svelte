55c2f81980b99cd34d1b95cb50e02f3ee2638c9c
n3-rd <godwinjdayo@gmail.com>
2023-08-13 11:44:48 +0100

    error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { page } from "$app/stores";
+</script>
+
+<div class="min-h-screen">
+    <div class="h1 text-center text-7xl">An error occoured</div>
+    <div
+        class="error-image-container w-full flex justify-center items-center py-4"
+    >
+        <div class="error-image">
+            <img src="/img/no-evidence.webp" alt="error" />
+        </div>
+    </div>
+
+    <div class="p text-center">
+        {$page.error.message}
+    </div>
+</div>
+
+<style>
+    img {
+        width: 20rem;
+        height: 20rem;
+        object-fit: cover;
+        object-position: center;
+    }
+</style>
