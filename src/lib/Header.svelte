a7f8f5c807fdf88eae09357a3109c5278189bf6e
n3-rd <godwinjdayo@gmail.com>
2023-08-13 10:58:14 +0100

    updated header links

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,10 +1,8 @@
 <div
     class="w-full fixed h-14 flex justify-between items-center px-7 bg-white rounded-b-xl z-50"
 >
-    <div class="title display">N3rd's Blog.</div>
+    <a href="/"><div class="title display">N3rd's Blog.</div></a>
     <div class="links flex justify-between gap-5">
-        <div class="li"><a href="dev.to">Link 1</a></div>
-        <div class="li"><a href="dev.to">Link 2</a></div>
-        <div class="li"><a href="dev.to">Link 3</a></div>
+        <div class="li">/dark-mode</div>
     </div>
 </div>
