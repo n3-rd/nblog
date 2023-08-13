ebde27582da52ac3812f0131ee9ba7495d9e3094
n3-rd <godwinjdayo@gmail.com>
2023-08-13 04:42:43 +0100

    added header

---
+++ src/lib/Header.svelte
@@ -0,0 +1,10 @@
+<div
+    class="w-screen fixed h-14 flex justify-between items-center px-7 bg-red-500"
+>
+    <div class="title">Blog</div>
+    <div class="links flex justify-between gap-5">
+        <div class="li"><a href="dev.to">Link 1</a></div>
+        <div class="li"><a href="dev.to">Link 2</a></div>
+        <div class="li"><a href="dev.to">Link 3</a></div>
+    </div>
+</div>
