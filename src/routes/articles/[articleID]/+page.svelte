dd385e26a6b48916f1560cb4d4ba0592842b7dbe
n3-rd <godwinjdayo@gmail.com>
2023-08-14 04:03:29 +0100

    seo

--- src/routes/articles/[articleID]/+page.svelte
+++ src/routes/articles/[articleID]/+page.svelte
@@ -9,6 +9,26 @@
 
 <svelte:head>
     <title>{article.title}</title>
+    <!-- image -->
+    <meta property="og:image" content={article.cover_image} />
+    <meta name="twitter:image" content={article.social_image} />
+    <!-- description -->
+    <meta name="description" content={article.description} />
+    <meta property="og:description" content={article.description} />
+    <meta name="twitter:description" content={article.description} />
+    <!-- title -->
+    <meta property="og:title" content={article.title} />
+    <meta name="twitter:title" content={article.title} />
+    <!-- url -->
+    <meta property="og:url" content={window.location.href} />
+    <meta name="twitter:url" content={window.location.href} />
+    <!-- type -->
+    <meta property="og:type" content="article" />
+    <!-- canonical url -->
+    <meta
+        property="og:article:published_time"
+        content={article.published_timestamp}
+    />
 </svelte:head>
 <ArticleImage coverImage={article.cover_image} title={article.title} />
 <ArticleInfo
