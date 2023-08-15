eda8e57a99b984dcaba3ba7e4d211bb1eb4fbed7
n3-rd <godwinjdayo@gmail.com>
2023-08-15 04:21:34 +0100

    added vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
   import "../app.css";
   import Header from "$lib/Header.svelte";
   import { theme } from "$lib/store.js";
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
+
+  inject({ mode: dev ? "development" : "production" });
 </script>
 
 <div class="{$theme} transition-colors min-h-screen">
