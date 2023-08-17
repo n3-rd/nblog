adeb0191a866056aa629c514150ce99d567a8e01
n3-rd <godwinjdayo@gmail.com>
2023-08-17 01:05:23 +0100

    Better Error Messages

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,7 +3,9 @@
 </script>
 
 <div class="min-h-screen">
-    <div class="h1 text-center text-7xl">An error occoured</div>
+    <div class="h1 text-center text-7xl">
+        {$page.status}: {$page.error.message}
+    </div>
     <div
         class="error-image-container w-full flex justify-center items-center py-4"
     >
@@ -12,9 +14,7 @@
         </div>
     </div>
 
-    <div class="p text-center">
-        {$page.error.message}
-    </div>
+    <div class="p text-center">An Error Occoured</div>
 </div>
 
 <style>
